036b11e8951da90759560accbfcfd2175ec4e9bf
HanMohamed <hmohamed2@seattleu.edu>
2024-01-24 17:57:09 -0800

    0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # 0. Create a file
 file { 'school':
-  path => '/tmp'
-  permission => '0744'
-  owner => 'www-data'
-  group => 'www-data'
+  path => '/tmp',
+  permission => '0744',
+  owner => 'www-data',
+  group => 'www-data',
   content => 'I love Puppet',
 }
