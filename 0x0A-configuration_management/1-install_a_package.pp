0a507db9a6a9777a883707f69a1aadb2b82d3ac3
HanMohamed <hmohamed2@seattleu.edu>
2024-01-25 20:57:43 -0800

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 #Using Puppet, install flask from pip3 version 2.1.0.
-package {'flask':
+package {'Werkzeug':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require => Package['Werkzeug']
 }
